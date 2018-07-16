5a39fdc01e7be1206e5e2b15fdb447a72ed8d47a
thorc <chelsgieck@gmail.com>
2018-07-16 19:12:12 -0400

    buggy

---
+++ routes/scrape.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cheerio = require('cheerio');
+const rp = require('request-promise');
+const router = express.Router();
+const db = require('../models');
+
+router.get("/newArticles", function(req, res) {
+  const options = {
+    uri: 'https://www.nytimes.com/section/us',
+    transform: function (body) {
+        return cheerio.load(body);
+    }
+  };
+  db.Article
+    .find({})
+    .then((savedArticles) => {
+      let savedHeadlines = savedArticles.map(article => article.headline);
+
+        rp(options)
+        .then(function ($) {
+          let newArticleArr = [];
+          //iterating over returned articles, and creating a newArticle object from the data
+          $('#latest-panel article.story.theme-summary').each((i, element) => {
+            let newArticle = new db.Article({
+              storyUrl: $(element).find('.story-body>.story-link').attr('href'),
+              headline: $(element).find('h2.headline').text().trim(),
+              summary : $(element).find('p.summary').text().trim(),
+              imgUrl  : $(element).find('img').attr('src'),
+              byLine  : $(element).find('p.byline').text().trim()
+            });
+            //checking to make sure newArticle contains a storyUrl
+            if (newArticle.storyUrl) {
+              //checking if new article matches any saved article, if not add it to array
+              //of new articles
+              if (!savedHeadlines.includes(newArticle.headline)) {
+                newArticleArr.push(newArticle);
+              }
+            }
+          });//end of each function
+
+          //adding all new articles to database
+          db.Article
+            .create(newArticleArr)
+            .then(result => res.json({count: newArticleArr.length}))//returning count of new articles to front end
+            .catch(err => {});
+        })
+        .catch(err => console.log(err)); //end of rp method
+    })
+    .catch(err => console.log(err)); //end of db.Article.find()
+});// end of get request to /scrape
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const path = require('path');
+const favicon = require('serve-favicon');
+
+const app = express();
+
+
+const config = require('./routes/database');
+mongoose.Promise = Promise;
+mongoose
+.connect(config.database)
+.then( result => {
+    console.log(`Connected '${result.connections[0].name}' on ${result.connections[0].host}:${result.connections[0].port}`);
+})
+.catch(err => console.log('This Error Occured:', err));
+// middleware 
+app.use(favicon(path.join(__dirname, 'public', './assets/images/favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// directories 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/articles',express.static(path.join(__dirname, 'public')));
+app.use('/notes',express.static(path.join(__dirname, 'public')));
+
+
+//routes
+const index = require('./routes/index');
+const articles = require('./routes/articles');
+const notes = require('./routes/notes');
+const scrape = require('./routes/scrape');
+
+app.use('/', index);
+app.use('/articles', articles);
+app.use('/notes', notes);
+app.use('/scrape', scrape);
+
+//starting server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
